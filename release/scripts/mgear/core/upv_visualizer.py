904246c6bc090e0aafeffd9e5d1da2f290a33cdd
miquelcampos <miquel.campos@gmail.com>
2026-04-07 10:56:03 +0900

    Core: UPV Visualizer: Replace Maya 2024.2+ nodes for backwards compat
    Replace normalize/crossProduct nodes with vectorProduct (operation
    modes 0 and 2) and max node with condition chain. These newer nodes
    only exist in Maya 2024.2+. Fix node naming to use .nodeName()
    instead of str(PyNode) which includes DAG path pipes causing
    invalid character warnings.

--- release/scripts/mgear/core/upv_visualizer.py
+++ release/scripts/mgear/core/upv_visualizer.py
@@ -44,17 +44,17 @@ def create_vector_subtraction_nodes(elbow, wrist, root, eff):
         dict: PMA nodes keyed by role.
     """
     names = {
-        "crossProduct_elbow": f"{elbow}_crossProduct",
-        "crossProduct_wrist": f"{wrist}_crossProduct",
-        "crossProduct_root": f"{root}_crossProduct",
-        "sub_elbow": str(elbow),
-        "sub_wrist": str(wrist),
-        "sub_eff": str(eff),
+        "crossProduct_elbow": "{}_crossProduct".format(elbow.nodeName()),
+        "crossProduct_wrist": "{}_crossProduct".format(wrist.nodeName()),
+        "crossProduct_root": "{}_crossProduct".format(root.nodeName()),
+        "sub_elbow": elbow.nodeName(),
+        "sub_wrist": wrist.nodeName(),
+        "sub_eff": eff.nodeName(),
     }
     nodes = {}
     for key, name in names.items():
         n = nod.createPlusMinusAverage3D([], operation=2)
-        pm.rename(n, f"{name}_pma")
+        pm.rename(n, "{}_pma".format(name))
         nodes[key] = n
     return nodes
 
@@ -124,22 +124,53 @@ def setup_math_operations(root, length_nodes, float_value=0.5):
     Returns:
         tuple: (half_one_float_node, math_nodes dict).
     """
-    max_float_node = nod.createMulNode(0.010, 1.0)
-    pm.rename(max_float_node, f"{root}_max_md")
+    # Compute the maximum of a minimum floor value and three
+    # vector lengths.  The 'max' node only exists in Maya
+    # 2024.2+, so we chain condition nodes instead.
+    min_floor = nod.createMulNode(0.010, 1.0)
+    pm.rename(min_floor, "{}_max_md".format(root.nodeName()))
 
-    max_node = pm.createNode("max", name=f"{root.name()}_max")
-    max_float_node.outputX >> max_node.input[0]
+    # max(floor, eff)
+    cond1 = nod.createConditionNode(
+        firstTerm="{}.outputX".format(min_floor.name()),
+        secondTerm=length_nodes["eff"].distance,
+        operator=2,  # greater than
+        ifTrue="{}.outputX".format(min_floor.name()),
+        ifFalse=length_nodes["eff"].distance,
+    )
+    pm.rename(cond1, "{}_max_cond1".format(root.nodeName()))
 
-    length_nodes["eff"].distance >> max_node.input[1]
-    length_nodes["elbow"].distance >> max_node.input[2]
-    length_nodes["wrist"].distance >> max_node.input[3]
+    # max(prev, elbow)
+    cond2 = nod.createConditionNode(
+        firstTerm="{}.outColorR".format(cond1.name()),
+        secondTerm=length_nodes["elbow"].distance,
+        operator=2,
+        ifTrue="{}.outColorR".format(cond1.name()),
+        ifFalse=length_nodes["elbow"].distance,
+    )
+    pm.rename(cond2, "{}_max_cond2".format(root.nodeName()))
+
+    # max(prev, wrist)
+    cond3 = nod.createConditionNode(
+        firstTerm="{}.outColorR".format(cond2.name()),
+        secondTerm=length_nodes["wrist"].distance,
+        operator=2,
+        ifTrue="{}.outColorR".format(cond2.name()),
+        ifFalse=length_nodes["wrist"].distance,
+    )
+    pm.rename(cond3, "{}_max_cond3".format(root.nodeName()))
 
     half_one_float_node = nod.createMulNode(
-        f"{max_node}.output", float_value
+        "{}.outColorR".format(cond3.name()), float_value
     )
-    pm.rename(half_one_float_node, f"{root}_half_one_md")
+    pm.rename(
+        half_one_float_node, "{}_half_one_md".format(root.nodeName())
+    )
 
-    math_nodes = {"max": max_node, "half_multiply": half_one_float_node}
+    math_nodes = {
+        "max_chain": [cond1, cond2, cond3],
+        "half_multiply": half_one_float_node,
+    }
 
     return half_one_float_node, math_nodes
 
@@ -162,41 +193,63 @@ def setup_cross_product_chain(root, elbow, wrist, vector_nodes, float_value):
         root, length_nodes, float_value
     )
 
+    # Use vectorProduct nodes instead of normalize/crossProduct
+    # which only exist in Maya 2024.2+.
+    # vectorProduct operation: 0=none, 1=dot, 2=cross, 3=vectorMatrixProduct
+    # normalizeOutput=True gives us normalized result.
+
+    # Normalize elbow and wrist cross products
     normalize_elbow = pm.createNode(
-        "normalize", name=f"{elbow}_normalize"
+        "vectorProduct",
+        name="{}_normalize".format(elbow.nodeName()),
     )
+    normalize_elbow.operation.set(0)
+    normalize_elbow.normalizeOutput.set(True)
+    vector_nodes["crossProduct_elbow"].output3D >> normalize_elbow.input1
+
     normalize_wrist = pm.createNode(
-        "normalize", name=f"{wrist}_normalize"
+        "vectorProduct",
+        name="{}_normalize".format(wrist.nodeName()),
     )
+    normalize_wrist.operation.set(0)
+    normalize_wrist.normalizeOutput.set(True)
+    vector_nodes["crossProduct_wrist"].output3D >> normalize_wrist.input1
 
-    vector_nodes["crossProduct_elbow"].output3D >> normalize_elbow.input
-    vector_nodes["crossProduct_wrist"].output3D >> normalize_wrist.input
-
+    # Cross product: wrist x elbow
     crossProduct_wrist_elbow = pm.createNode(
-        "crossProduct", name=f"{root}_crossProduct_wrist_elbow"
+        "vectorProduct",
+        name="{}_crossProduct_wrist_elbow".format(root.nodeName()),
     )
-    crossProduct_default = pm.createNode(
-        "crossProduct", name=f"{root}_crossProduct_default"
-    )
-    crossProduct_default.input2Z.set(-1.000)
-
+    crossProduct_wrist_elbow.operation.set(2)
     normalize_wrist.output >> crossProduct_wrist_elbow.input1
     normalize_elbow.output >> crossProduct_wrist_elbow.input2
+
+    # Default cross product (elbow x -Z)
+    crossProduct_default = pm.createNode(
+        "vectorProduct",
+        name="{}_crossProduct_default".format(root.nodeName()),
+    )
+    crossProduct_default.operation.set(2)
+    crossProduct_default.input2Z.set(-1.000)
     normalize_elbow.output >> crossProduct_default.input1
 
     # Sum cross product components to check if zero
     cp_sum_node = nod.createPlusMinusAverage1D(
         [
-            f"{crossProduct_wrist_elbow}.outputX",
-            f"{crossProduct_wrist_elbow}.outputY",
-            f"{crossProduct_wrist_elbow}.outputZ",
+            "{}.outputX".format(crossProduct_wrist_elbow),
+            "{}.outputY".format(crossProduct_wrist_elbow),
+            "{}.outputZ".format(crossProduct_wrist_elbow),
         ],
         operation=1,
     )
-    pm.rename(cp_sum_node, f"{root}_crossProduct_wrist_elbow_sum")
+    pm.rename(
+        cp_sum_node,
+        "{}_crossProduct_wrist_elbow_sum".format(root.nodeName()),
+    )
 
     condition_node = pm.createNode(
-        "condition", name=f"{root}_condition"
+        "condition",
+        name="{}_condition".format(root.nodeName()),
     )
     condition_node.secondTerm.set(0.000)
 
@@ -204,21 +257,32 @@ def setup_cross_product_chain(root, elbow, wrist, vector_nodes, float_value):
     crossProduct_default.output >> condition_node.colorIfTrue
     crossProduct_wrist_elbow.output >> condition_node.colorIfFalse
 
+    # Normalize condition output
     normalize_condition_node = pm.createNode(
-        "normalize", name=f"{root}_normalize_condition"
+        "vectorProduct",
+        name="{}_normalize_condition".format(root.nodeName()),
     )
-    condition_node.outColor >> normalize_condition_node.input
+    normalize_condition_node.operation.set(0)
+    normalize_condition_node.normalizeOutput.set(True)
+    condition_node.outColor >> normalize_condition_node.input1
 
+    # Final cross product: condition x root
     crossProduct_root = pm.createNode(
-        "crossProduct", name=f"{root}_crossProduct_root"
+        "vectorProduct",
+        name="{}_crossProduct_root".format(root.nodeName()),
     )
+    crossProduct_root.operation.set(2)
     normalize_condition_node.output >> crossProduct_root.input1
     vector_nodes["crossProduct_root"].output3D >> crossProduct_root.input2
 
+    # Normalize final result
     crossProduct_root_normalize_node = pm.createNode(
-        "normalize", name=f"{root}_crossProduct_root_normalize"
+        "vectorProduct",
+        name="{}_crossProduct_root_normalize".format(root.nodeName()),
     )
-    crossProduct_root.output >> crossProduct_root_normalize_node.input
+    crossProduct_root_normalize_node.operation.set(0)
+    crossProduct_root_normalize_node.normalizeOutput.set(True)
+    crossProduct_root.output >> crossProduct_root_normalize_node.input1
 
     return crossProduct_root_normalize_node, half_multiply_node, math_nodes
 
@@ -247,16 +311,16 @@ def setup_upv_position_calculation(
             f"{half_multiply_node}.outputX",
         ],
     )
-    pm.rename(upv_mul, f"{elbow}_upv_pos_multiply")
+    pm.rename(upv_mul, "{}_upv_pos_multiply".format(elbow.nodeName()))
 
     upv_sum = nod.createPlusMinusAverage3D(
         [
-            f"{upv_mul}.output",
-            f"{decompose_nodes[1]}.outputTranslate",
+            "{}.output".format(upv_mul),
+            "{}.outputTranslate".format(decompose_nodes[1]),
         ],
         operation=1,
     )
-    pm.rename(upv_sum, f"{elbow}_upv_pos_pma")
+    pm.rename(upv_sum, "{}_upv_pos_pma".format(elbow.nodeName()))
 
     upv_sum.output3Dx >> upv.translateX
     upv_sum.output3Dy >> upv.translateY
